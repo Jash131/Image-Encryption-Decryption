3d5b502896569a12a8babaa1f3c979020cb9a7d1
Jash131 <jayaprakash2k4@gmail.com>
2024-09-10 21:16:33 +0530

    create main.dart

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:imageencdec/ImageEncryptionApp.dart';
+import 'package:imageencdec/SplashScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Image ecrypt decrypt',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
